c74fc53d968b88b32c17f795cef3e17d5ce9490f
vbruni-d <88150027+vbruni-d@users.noreply.github.com>
2021-09-05 23:04:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
             <div id="application-MarketingContent-manage-component---ObjectView--DesignView--ContentPage-0L-1W" data-sap-cp-key="E4A13AEAA31960EB67AA290B0B3F70AF4931800A" class="sapCpWidget sapCRLWidget sapCpInputWidget sapCpWidgetMandatory sapCpContactAttribute-YY1_RUT_ENH" data-sap-cp-wprogres-enabled="false">
                 <div class="sapCpWidgetContent sapCpWidgetContentLeft sapCRLWidgetContent sapCRLWidgetContentLeft" ><label id="__label49" class="sapCpLabel sapCpLabelRequired input-lbl" for="__input25">Rut</label></div>
                 <div class="sapCpWidgetContent sapCpWidgetContentLeft sapCRLWidgetContent sapCRLWidgetContentLeft">
-                    <input id="__input25" class="sapCpInput txt-input" type="text" required="required" maxlength="10" required onkeyup="checkRut(this)">
+                    <input id="__input25" class="sapCpInput txt-input" type="text" required="required" maxlength="10" oninput="checkRut(this)" name="rut">
                 </div>
                 <div class="sapCpWidgetFixContent sapCRLWidgetFixContent"></div>
             </div>
@@ -197,50 +197,49 @@
 <script>
 
     function checkRut(rut) {
-
         // Despejar Puntos
-        var valor = rut.value.replace('.', '');
+        var valor = rut.value.replace('.','');
         // Despejar Guión
-        valor = valor.replace('-', '');
-
+        valor = valor.replace('-','');
+        
         // Aislar Cuerpo y Dígito Verificador
-        cuerpo = valor.slice(0, -1);
+        cuerpo = valor.slice(0,-1);
         dv = valor.slice(-1).toUpperCase();
-
+        
         // Formatear RUN
-        rut.value = cuerpo + '-' + dv
-
+        rut.value = cuerpo + '-'+ dv
+        
         // Si no cumple con el mínimo ej. (n.nnn.nnn)
-        if (cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false; }
-
+        if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
+        
         // Calcular Dígito Verificador
         suma = 0;
         multiplo = 2;
-
+        
         // Para cada dígito del Cuerpo
-        for (i = 1; i <= cuerpo.length; i++) {
-
+        for(i=1;i<=cuerpo.length;i++) {
+        
             // Obtener su Producto con el Múltiplo Correspondiente
             index = multiplo * valor.charAt(cuerpo.length - i);
-
+            
             // Sumar al Contador General
             suma = suma + index;
-
+            
             // Consolidar Múltiplo dentro del rango [2,7]
-            if (multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
-
+            if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+    
         }
-
+        
         // Calcular Dígito Verificador en base al Módulo 11
         dvEsperado = 11 - (suma % 11);
-
+        
         // Casos Especiales (0 y K)
-        dv = (dv == 'K') ? 10 : dv;
-        dv = (dv == 0) ? 11 : dv;
-
+        dv = (dv == 'K')?10:dv;
+        dv = (dv == 0)?11:dv;
+        
         // Validar que el Cuerpo coincide con su Dígito Verificador
-        if (dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
-
+        if(dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+        
         // Si todo sale bien, eliminar errores (decretar que es válido)
         rut.setCustomValidity('');
     }
