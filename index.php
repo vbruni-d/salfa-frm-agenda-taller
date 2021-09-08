648a215b7f28ab93a4999140feff7d5cda35e633
vbruni-d <88150027+vbruni-d@users.noreply.github.com>
2021-09-08 19:04:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -273,33 +273,7 @@
         });
     });
 
-    $(function() {
-
-        $('#__input26').keydown(function (e) {
-
-            if (e.shiftKey || e.ctrlKey || e.altKey) {
-
-                e.preventDefault();
-
-            } else {
-
-                var key = e.keyCode;
-
-                if (!((key == 8) || (key == 46) || (key >= 35 && key <= 40) || (key >= 48 && key <= 57) || (key == 107) || (key >= 96 && key <= 105) )) {
-
-                    e.preventDefault();
-
-                }
-
-            }
-
-        });
-
-    });
-
-
-
-
+    
 </script>
 
 
